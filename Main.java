4202ce19ad61e9df04cc84144c017a1733b233b6
Sapriadi <147253388+Sapriadiadi@users.noreply.github.com>
2023-10-15 20:40:16 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+package days7;
+
+public class Main {
+    
+    public static void main(String[] args){
+        
+
+        int num = 4;
+        for(int i = 1; i <= 10; ++i)
+        
+            System.out.printf("%d * %d = %d \n", num, i, num * i);
+        }
+    
+
+        
+    
+    
+    
+    }
+    
+
